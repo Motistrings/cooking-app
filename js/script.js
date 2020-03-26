03db41a2603c914e39e368dc1d9f7ff63839aed6
Motistrings <timgrand4929@gmail.com>
2020-03-26 14:27:00 +0300

    added back-end code for gallons to litres conversion

---
+++ js/script.js
@@ -0,0 +1,13 @@
+
+var gallons_litres=function(gallons){
+    return gallons*3.78541;
+};
+var gallons=parseInt(prompt("Enter number of gallons"));
+alert("Result is: "+gallons*3.78541+ "litres");
+
+var litres_gallons=function(litres){
+    return litres*0.264172;
+};
+var litres=parseInt(prompt("Enter number of litres"));
+alert("Result is: "+litres*0.264172+ "gal");
+
